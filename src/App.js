d58219f02ee4cda722f6180e64b5a9d47d4a3db8
Edwin Choi <edwinchoi1@gmail.com>
2021-11-13 15:15:45 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
-
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import Contacts from './components/contacts/Contacts'
+import Banner from './components/banner/Banner'
 import './App.css';
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
 
-  useEffect(() =>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setIsLoaded(true);
-        setItems(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    )
-  },[])
-  if(error){
-    return <div>Error: {error.message}</div>;
-  }else if (!isLoaded){
-    return <div>Loading...</div>;
-  }else {
     return (
+      
       <div className="App">
+        <Banner />
         <header className="App-header">
           <p>
-            <code></code>
-            {items.map(item => (
-            <li key={item.id}>
-              {item.name} {item.username} {item.email} 
-            </li>
-            ))}
+            <Contacts />
           </p>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
